b13e39b047ff93c66e1cb09165473a1bc30749b2
Victor Rojas Centeno <vico.16c@gmail.com>
2019-12-04 15:23:50 -0500

    cambio

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # RSYNC.SH CODDEPLOY
-rsync -avz --delete-after --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/* /var/www/api.ibuy.pe/
+rsync -avz --delete-after --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/ /var/www/api.ibuy.pe/
 chmod -R 775 /var/www/api.ibuy.pe
 chown -R apache:apache /var/www/api.ibuy.pe
 #cd /dataweb/libero/3.libero.pe
