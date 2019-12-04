2ddc3596d5337c29c1401fe3ea34b203b191d94f
Victor Rojas Centeno <vico.16c@gmail.com>
2019-12-04 14:28:57 -0500

    cambio

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # RSYNC.SH CODDEPLOY
-rsync -avz --delete-after --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/* /var/www/html/galletalaravel/
-chmod -R 775 /var/www/html/galletalaravel
-chown -R www-data:www-data /var/www/html/galletalaravel
+rsync -avz --delete-after --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/* /var/www/api.ibuy.pe/
+chmod -R 775 /var/www/api.ibuy.pe
+chown -R apache:apache /var/www/api.ibuy.pe
 #cd /dataweb/libero/3.libero.pe
 #aws s3 sync --profile default --region us-east-1  /dataweb/libero/3.libero.pe/public/ s3://libero-media/public/
 #composer update
