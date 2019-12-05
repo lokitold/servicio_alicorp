8fe6c3c2c66529de07d17edf94b98fcf08867ac8
Victor Rojas Centeno <vico.16c@gmail.com>
2019-12-05 17:20:16 -0500

    cambio

--- scripts/rsync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# RSYNC.SH CODDEPLOY
-#rsync -avz --delete-after --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/ /var/www/api.ibuy.pe/
-rsync -ahvz --update --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/ /var/www/api.ibuy.pe/
-chmod -R 775 /var/www/api.ibuy.pe
-chown -R apache:apache /var/www/api.ibuy.pe
-#cd /dataweb/libero/3.libero.pe
-#aws s3 sync --profile default --region us-east-1  /dataweb/libero/3.libero.pe/public/ s3://libero-media/public/
-#composer update
